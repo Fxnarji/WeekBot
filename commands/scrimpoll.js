b1437736bdee6172081e192ed30d15dd3679b607
Fxnarji <92873527+Fxnarji@users.noreply.github.com>
2024-01-10 20:41:07 +0000

    corrected role in /scrimpoll

--- commands/scrimpoll.js
+++ commands/scrimpoll.js
@@ -8,7 +8,7 @@ module.exports = {
 
     async execute(interaction) {
         await interaction.deferReply({ephemeral:true})
-        await reactWeekdays("<@&1191886949221408858> an welchen Wochentagen habt ihr nächste Woche Zeit? \n Wie immer, 20 - 22 Uhr", interaction.channel)
+        await reactWeekdays("<@&895340735333806150> an welchen Wochentagen habt ihr nächste Woche Zeit? \n Wie immer, 20 - 22 Uhr", interaction.channel)
         await interaction.followUp({ content: 'Succesfully asked for DGE scrim! (Hope people show up)', ephemeral: true });
     }
 
